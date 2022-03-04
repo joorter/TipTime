b9f87f9c1259f1dc2e22e1c7a851b7e54341f6a0
joorter <josean1799@gmail.com>
2022-03-04 18:23:35 +0100

    Ej 6

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -18,15 +18,15 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun calculateTip() {
-        val stringInTextField = binding.costOfService.text.toString()
-        val cost = stringInTextField.toDouble()
+        val stringInTextField = binding.costOfServiceEditText.text.toString()
+        val cost = stringInTextField.toDoubleOrNull()
         val selectedId = binding.tipOptions.checkedRadioButtonId
         val tipPercentage = when (selectedId) {
             R.id.option_twenty_percent -> 0.20
             R.id.option_eighteen_percent -> 0.18
             else -> 0.15
         }
-        var tip = tipPercentage * cost
+        var tip = tipPercentage * cost!!
         val roundUp = binding.roundUpSwitch.isChecked
         if (roundUp) {
             tip = kotlin.math.ceil(tip)
